419a76b88d5de8725a9ce3939da4795c7d574071
Alfonso Torres Echave <hurbogt522o@gmail.com>
2016-10-16 13:32:03 -0600

    pdf creator

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,13 +47,64 @@
   </div>
 
 
-
-
+  <div id="customers">
+      <table id="tab_customers" class="table table-striped">
+          <colgroup>
+              <col width="20%">
+                  <col width="20%">
+                      <col width="20%">
+                          <col width="20%">
+          </colgroup>
+          <thead>
+              <tr class='warning'>
+                  <th>Country</th>
+                  <th>Population</th>
+                  <th>Date</th>
+                  <th>Age</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td>Chinna</td>
+                  <td>1,363,480,000</td>
+                  <td>March 24, 2014</td>
+                  <td>19.1</td>
+              </tr>
+              <tr>
+                  <td>India</td>
+                  <td>1,241,900,000</td>
+                  <td>March 24, 2014</td>
+                  <td>17.4</td>
+              </tr>
+              <tr>
+                  <td>United States</td>
+                  <td>317,746,000</td>
+                  <td>March 24, 2014</td>
+                  <td>4.44</td>
+              </tr>
+              <tr>
+                  <td>Indonesia</td>
+                  <td>249,866,000</td>
+                  <td>July 1, 2013</td>
+                  <td>3.49</td>
+              </tr>
+              <tr>
+                  <td>Brazil</td>
+                  <td>201,032,714</td>
+                  <td>July 1, 2013</td>
+                  <td>2.81</td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+  <button onclick="javascript:demoFromHTML();">PDF</button>
 
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <!-- Compiled and minified JavaScript -->
   <script src="/js/materialize.min.js"></script>
   <script src="/js/init.js"></script>
+  <script src="/js/jspdf.min.js"></script>
+
 </body>
 </html>
